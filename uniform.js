de0f98897ef6d83406ba3b6c10180ff6edc3ce0d
Peter Day <peterday.main@gmail.com>
2020-07-02 14:47:03 +0100

    Update uniform.js

--- uniform.js
+++ uniform.js
@@ -2,13 +2,8 @@ var Uniform = {
     Boot:function(){
         var b=0;
         $( "*[data-uniform=true]" ).each(function() {
-            if($(this).height()>b){
-                b = $(this).height();
-            }
+            b = $(this).height()>b ? $(this).height() : b;
         });
         $( "*[data-uniform=true]" ).height(b);
     }
-};
-$(function() {
-    Uniform.Boot();
-});
+};Uniform.Boot();
